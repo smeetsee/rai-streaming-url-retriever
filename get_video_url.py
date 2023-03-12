a9c836d17ca3407103c7a371e0c592b2a2e7da5a
Xander Smeets <xander@smeets.re>
2023-03-12 17:16:01 +0100

    Add headers

--- get_video_url.py
+++ get_video_url.py
@@ -15,7 +15,13 @@ def get_channel_m3u8(url):
     elif 'rai5' == url:
         url = 'https://mediapolis.rai.it/relinker/relinkerServlet.htm?cont=395276&output=64'
     
-    response = requests.get(url)
+    response = requests.get(url, headers={
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Safari/605.1.15',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language': 'nl-NL,nl;q=0.9'#,
+        #'Connection': 'keep-alive',
+        #'Accept-Encoding': 'gzip, deflate, br'
+        })
     if response.status_code == 403:
         return 403
     root = ET.ElementTree(response.content).getroot()
